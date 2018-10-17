94eb88c25da88376f4402b8402142f564b2d7db5
99darwin <nicksapsales@gmail.com>
2018-10-17 16:48:07 -0700

    undefined array handling

--- src/routing/api.js
+++ src/routing/api.js
@@ -405,7 +405,7 @@ module.exports = (app) => {
                 if (count == addresses.length) {
                     let uniqAddresses = _.uniq(addresses)
                     uniqAddresses.forEach(uniqAddress => {
-                        if (uniqAddress !== null) {
+                        if (uniqAddress !== [ undefined ]) {
                             let newBalanceObj = {
                                 "addresses": [uniqAddress]
                             }
